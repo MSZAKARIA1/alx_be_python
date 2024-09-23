87eab8fd6d335b44a7fd280546924a53f5e9f49a
mahama saani zakaria <zakariamahamasaani@gmail.com>
2024-09-23 13:33:54 -0700

    add arithmatic operations file

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,6 +2,6 @@ size = int(input("Enter the size of the pattern:"))
 original_size = size
 while size != 0:
     for i in range(original_size):
-        print("*", end="")
+        print("*", end=" ")
     size -= 1
     print()
